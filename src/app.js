72d25f894d0e2de81589e4f43d2db35620d91cc6
sohadQtaitat <qtaitat3@gmail.com>
2020-03-12 21:40:19 +0200

    done

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import ToDo from './components/todo/todo-connected.js';
-import Header from './components/todo/header.js';
-import Footer from './components/todo/footer.js';
+import Header from './components/header/header.js';
+import Footer from './components/footer/footer.js';
 function App() {
   return (
     <>
